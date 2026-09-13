d6d6cecfdade409eae73d82ffe2f5cc07ea639d3
Hisyam Prasetyo <hisyamprsetyo@gmail.com>
2026-09-13 20:55:09 +0700

    fix(Experience): Fix wrong name

--- main/views.py
+++ main/views.py
@@ -17,7 +17,7 @@ def show_main(request):
 
 def show_experience(request):
     context = {
-        "name": "Burhan",
+        "name": "Hisyam",
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
