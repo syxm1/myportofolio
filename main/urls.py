244409b6652757127e4eb860c77c06f0d86af8ec
Hisyam Prasetyo <hisyamprsetyo@gmail.com>
2026-09-16 22:07:23 +0700

    feat: Implement data delivery with JSON

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_education, create_education, create_experience
+from main.views import (
+    show_main, 
+    show_experience, 
+    show_education, 
+    create_education, 
+    create_experience,
+    get_education_json,
+    get_experience_json,
+    delete_education,
+    delete_experience,
+)
 
 app_name = "main"
 
@@ -10,4 +20,8 @@ urlpatterns = [
     path("education/", show_education, name="show_education"),
     path("experience/add/", create_experience, name="create_experience"),
     path("education/add/", create_education, name="create_education"),
+    path("api/experience", get_experience_json, name="get_experience_json"),
+    path("api/education", get_education_json, name="get_education_json"),
+    path("experience/<uuid:experience_id>/delete/",delete_experience,name="delete_experience"),
+    path("education/<uuid:education_id>/delete/",delete_education,name="delete_education")
 ]
--- main/views.py
+++ main/views.py
@@ -17,19 +17,42 @@ def show_main(request):
     }
     return render(request, "index.html", context)
 
-
 def show_experience(request):
+    json_response = get_experience_json(request)
+
+    experiences = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+
+    experiences = [experience.object for experience in experiences]
+    title_query = request.GET.get("title", "").strip()
+
     context = {
         "name": "Hisyam",
-        "experience_list": Experience.objects.all(),
+        "experience_list": experiences,
+        "title_query": title_query,
     }
+    
     return render(request, "experience.html", context)
 
 def show_education(request):
+    json_response = get_education_json(request)
+
+    educations = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+
+    educations = [education.object for education in educations]
+    institution_query = request.GET.get("institution", "").strip()
+
     context = {
         "name": "Hisyam",
-        "education_list": Education.objects.all(),
+        "education_list": educations,
+        "institution_query": institution_query,
     }
+    
     return render(request, "education.html", context)
 
 def create_experience(request):
@@ -59,3 +82,43 @@ def create_education(request):
         "form": form,
     }
     return render(request, "education_form.html", context)
+
+def get_education_json(request):
+    institution_query = request.GET.get("institution", "").strip()
+    educations = Education.objects.all()
+
+    if institution_query:
+        educations = educations.filter(institution__icontains=institution_query)
+
+    educations_json = serializers.serialize("json", educations)
+    return HttpResponse(educations_json, content_type="application/json")
+
+def get_experience_json(request):
+    title_query = request.GET.get("title", "").strip()
+    experience = Experience.objects.all()
+
+    if title_query:
+        experience = experience.filter(title__icontains=title_query)
+
+    experience_json = serializers.serialize("json", experience)
+    return HttpResponse(experience_json, content_type="application/json")
+
+def delete_experience(request, experience_id):
+    experience = get_object_or_404(Experience, pk=experience_id)
+
+    if request.method == "POST":
+        experience.delete()
+        messages.success(request, "Experience berhasil dihapus!")
+        return redirect("main:show_experience")
+
+    return redirect("main:show_experience")
+
+def delete_education(request, education_id):
+    education = get_object_or_404(Education, pk=education_id)
+
+    if request.method == "POST":
+        education.delete()
+        messages.success(request, "Education berhasil dihapus!")
+        return redirect("main:show_education")
+
+    return redirect("main:show_education")
